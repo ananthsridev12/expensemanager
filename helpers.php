96bbeca72bbbc5b4e886479e71270f1d38805af6
ananthsridev <ananth@dhanlap.com>
2026-03-04 20:54:57 +0530

    Use rupee symbol for currency helper

--- helpers.php
+++ helpers.php
@@ -4,6 +4,6 @@ if (!function_exists('formatCurrency')) {
     function formatCurrency($value): string
     {
         $amount = is_numeric($value) ? (float) $value : 0;
-        return '? ' . number_format($amount, 2);
+        return '₹ ' . number_format($amount, 2);
     }
 }
