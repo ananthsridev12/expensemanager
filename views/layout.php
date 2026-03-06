b1c6e86f604edead055b02a60e14d78dceb72601
ananthsridev <ananth@dhanlap.com>
2026-03-06 10:48:14 +0530

    Fix mobile menu toggle by using external JS and cache-busted assets

--- views/layout.php
+++ views/layout.php
@@ -13,10 +13,10 @@ if (!function_exists('formatCurrency')) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Personal Finance Manager</title>
-    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="public/css/style.css?v=<?= filemtime(__DIR__ . '/../public/css/style.css') ?>">
 </head>
 <body>
     <?= $content ?? '' ?>
-    <script src="public/js/main.js"></script>
+    <script src="public/js/main.js?v=<?= filemtime(__DIR__ . '/../public/js/main.js') ?>"></script>
 </body>
 </html>
